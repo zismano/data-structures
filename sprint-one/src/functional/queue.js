26cdb997552ad304fd8e6cb21aa5c4c0b04ca605
Moises <me@moisesmunoz.net>
2017-11-29 11:16:08 -0800

    Completed enqueue and dequeue function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,5 @@
 var Queue = function() {
+  var sz = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -7,12 +8,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add a string to the back of the queue
+    storage[sz] = value;
+    sz++;
   };
 
   someInstance.dequeue = function() {
+    // remove and return the str at the front of the queue
+    var temp = storage[0] || '';
+    if (delete storage[0]) {
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    // return the number of items in the queue
   };
 
   return someInstance;
