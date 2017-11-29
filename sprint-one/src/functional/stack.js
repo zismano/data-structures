dde432126bf71611a67d2966bea49e8198f41e5c
Moises <me@moisesmunoz.net>
2017-11-29 12:26:11 -0800

    [When this code is commited it will] Complete the functional stack push, pop, size functions and pass its tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function() {
+  var sz = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,12 +7,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Add a string to the top of the stack
+    someInstance[sz] = value;
+    sz++;
   };
 
   someInstance.pop = function() {
+    // Remove and return the string on the top of the stack
+
+    var temp = someInstance[sz-1];
+    console.log(temp);
+    if (delete someInstance[sz-1]) {
+      if (sz > 0) {
+        sz--;
+      }
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    // Return the number of items on the stack
+    return sz;
   };
 
   return someInstance;
