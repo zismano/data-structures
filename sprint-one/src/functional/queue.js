ae4f542b3dc0e8540ad0bd4f4e35a650b0a332bc
Moises <me@moisesmunoz.net>
2017-11-29 11:54:06 -0800

    [When this code is commited it will] Complete the size method and pass all tests for functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -9,20 +9,29 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     // add a string to the back of the queue
-    storage[sz] = value;
+    someInstance[sz] = value;
     sz++;
   };
 
   someInstance.dequeue = function() {
     // remove and return the str at the front of the queue
-    var temp = storage[0] || '';
-    if (delete storage[0]) {
+    var i = 0;
+    var temp = someInstance[0];
+    if (delete someInstance[0]) {
+      if (sz > 0) {
+        sz--;
+      }
+      for (var key in someInstance) {
+        someInstance[i] = someInstance[key];
+        i++;
+      }
       return temp;
     }
   };
 
   someInstance.size = function() {
     // return the number of items in the queue
+    return sz;
   };
 
   return someInstance;
