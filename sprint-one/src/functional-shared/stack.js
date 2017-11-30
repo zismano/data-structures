91af2f67c3cb12afa9bc3b57605aa8719441df3e
zismano <zismanofir@gmail.com>
2017-11-29 17:20:13 -0800

    'Implemented push and size methods'

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackMaker = {};
+  stackMaker.sz = 0;
+  _.extend(stackMaker, stackMethods);
+
+  return stackMaker;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  this[this.sz] = val;
+  this.sz++;
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return this.sz;
+};
+
+// var st = Stack();
+// st.push(5)
 
