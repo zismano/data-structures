0bfabfc9815c9f9a12a50383e4c2950e9d35cdf5
zismano <zismanofir@gmail.com>
2017-11-29 18:26:32 -0800

    'Finished Prototypal Stack and make progress on Queue'

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueMaker = Object.create(queueMethods);
+  queueMaker.sz = 0;
+  return queueMaker;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  // this[this.sz] = val;
+  // this.sz++;
+};
+
+queueMethods.dequeue = function() {
+  var temp = this[this.sz - 1];
+  //if (temp)
+};
+
+queueMethods.size = function() {
+  return this.sz;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackMaker = Object.create(stackMethods);
+  stackMaker.sz = 0;
+  return stackMaker;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(val) {
+  this[this.sz] = val;
+  this.sz++;
+};
+
+stackMethods.pop = function() {
+  var temp = this[this.sz - 1];
+  if (temp) {
+    delete this[this.sz - 1];
+    this.sz--;
+    return temp;
+  }
+};
+
+stackMethods.size = function() {
+  return this.sz;
+};
 
 
