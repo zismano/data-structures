24faa6cd1f10db437c1942e62d6a188bdd18168f
Ofir Zisman <31937034+zismano@users.noreply.github.com>
2017-12-01 10:31:27 -0800

    Added class and pseudocode for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,45 @@
 var BinarySearchTree = function(value) {
+	var obj = {};
+	obj.value = value;
+	obj.left = null;
+	obj.right = null;
+	obj = Object.create(BinarySearchTree.prototype);
+	return obj;
 };
 
+// accepts a value and places in the tree in the correct position.
+BinarySearchTree.prototype.insert = function(value) {
+  // newNode = { value: value, left : null, right : null }
+  // if value < tree-root.value
+	// if tree-root.left is not null
+	  // this.left.insert(value)
+	// else
+	  // root.left = newNode
+  // else then value > tree
+	// if tree-root.right is not null
+	  // this.right.insert(value)
+	// else
+	  // root.right = newNode
+  // else (equals) return;		
+};
 
+// accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+BinarySearchTree.prototype.contains = function(value) {
+  // if value === tree.root.value
+	// return true
+  // else if value < tree.root.value
+	// if tree.root.left  
+	  // this.left.contains(value)
+	// else return false
+  // else if value > tree.root.value
+	// if tree.root.right
+	  // this.right.contains(value)
+	// else
+	  // return false
+};
+// accepts a callback and executes it on every value contained in the tree.
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
