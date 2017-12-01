60ca756e0ea13477041c889ccd6b6c2ae5d40f1b
zismano <zismanofir@gmail.com>
2017-11-30 21:12:24 -0800

    Finished Implementing Set function and its' methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me (should be array)
   return set;
 };
 
 var setPrototype = {};
 
+// complexity is constant
+// takes any string and adds it to the set
 setPrototype.add = function(item) {
+  // new property inside storage (item : true)
+  this._storage[item] = true;
 };
 
+// complexity is constant
+// takes any string and returns a boolean reflecting whether it can be found in the set
 setPrototype.contains = function(item) {
+  // if key item in set, return true. Otherwise, return false
+  return this._storage[item] ? true : false;
 };
 
+// complexity is constant
+// takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
+  // delete key item
+  delete this._storage[item];
 };
 
 /*
