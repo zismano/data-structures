29460005a040d63042a4098ca9d6f8d1ac607145
zismano <zismanofir@gmail.com>
2017-12-01 18:45:38 -0800

    Finished implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,9 +11,9 @@ HashTable.prototype.insert = function(k, v) {
   // create new tuple = [k, v]
   var newTuple = [k, v];
   var bucket = this._storage.get(index);
-  // check this.get[index] is undefined
+  // check if this.get[index] is undefined
   if (bucket === undefined) {
-    // if so, create new bucket        // push new tuple to new bucket 
+    // if so, create new bucket and push new tuple to new bucket 
     this._storage.set(index, [newTuple]);
   } else {
     // else, push new tuple to existing bucket
@@ -33,19 +33,24 @@ HashTable.prototype.insert = function(k, v) {
   // check if tupleCt/limit > 75%
   if (this.tupleCt / this._limit * 100 >= 75) {
     //  if so, create new array and push all tuples in
-    reallocateTuples('increase'); 
+    this.reallocateTuples('increase'); 
   }
 };
 
-var reallocateTuples = function(slope) {
+HashTable.prototype.reallocateTuples = function(slope) {
   var allTuples = [];  
   this._storage.each(function(bucket) {
-    bucket.each(function(tuple) {
-      allTuples.push(tuple);
-    });
-    bucket = [];
+    if (bucket !== undefined) {
+      _.each(bucket, function(tuple) {
+        allTuples.push(tuple);
+      });
+      bucket = [];    
+    }
   });
+  this.tupleCt = 0;
   this._limit *= slope === 'increase' ? 2 : 0.5;
+
+  this._storage = LimitedArray(this._limit);
   var hashT = this;
   _.each(allTuples, function(item) {
     hashT.insert(item[0], item[1]);
@@ -55,26 +60,33 @@ var reallocateTuples = function(slope) {
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var buckets = this._storage.get(index);
-  for (var i = 0; i < buckets.length; i++) {
+  for (var i = 0; buckets && i < buckets.length; i++) {
     if (buckets[i][0] === k) {
       return buckets[i][1];
     }
   }
-  return false;
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // get buckets of storage's index
+  var bucket = this._storage.get(index);
   // iterate on buckets array to find tuple with k key
-  // pop tuple from bucket
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      // delete tuple from bucket
+      bucket.splice(i, 1);
+      break;  
+    }
+  }
   // decrease tuple counter
+  this.tupleCt--;
   // check if ratio tuples and storage length is less than 25%
-    // if so, create new array and push all tuples in
-    // delete all buckets
-    // divide limit by 2
-    // insert all tuples
-
+  // new limit of storage, and reassign tuples
+  if (this.tupleCt / this._limit * 100 < 25) {
+    this.reallocateTuples('decrease');
+  }
 };
 
 
