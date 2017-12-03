307e200feaacfb9675599f2072932109d5756a60
zismano <zismanofir@gmail.com>
2017-12-02 16:46:02 -0800

    Refactored code

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,38 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   // running time O(1)
-  list.addToTail = function(value) {
-    var newTail = Node(value);
-    // if (!head) then head is newNode
-    if (!list.head) {
-      list.head = newTail;
-    } else {
-      // else we need to connect to last tail
-      list.tail.next = newTail;
-    }
+  list.addToTail = value => {
+    let newTail = Node(value);
+    // if (!head) then head is newNode else we need to connect to last tail
+    !list.head ? list.head = newTail : list.tail.next = newTail;
     // newNode will always become tail
     list.tail = newTail;
   };
 
-  // running time O(1)
-  list.removeHead = function() {
-    // removes the first node from the list and returns its value
+  // removes the first node from the list and returns its value [running time O(1)]
+  list.removeHead = () => {
     if (list.head === null) {
       return null;
     }
     // create temp var that will be value of current head
-    var head = list.head;
+    let head = list.head;
     // list.head will be head.next
     list.head = list.head.next;
     // return temp
     return head.value;
   };
 
-  // running time O(n)
-  list.contains = function(target) {
-    // returns boolean reflecting whether or not the passed-in value is in the linked list
+  // returns boolean reflecting whether or not the passed-in value is in the linked list [running time O(n)]
+  list.contains = target => {
     // var node is head of list
-    var node = list.head;
+    let node = list.head;
     // do node evaluation vs passed-in value while node is not equal to null
     do {
       // if match return true else at the end of iteration return false
@@ -47,23 +40,19 @@ var LinkedList = function() {
     return false;
   };
 
-  // added a function to additional test 
-  list.addValueBeforeValue = function(previousValue, newValue) {
-    var newNode = Node(newValue);
-    for (var head = list.head; head.value === previousValue; head = head.next) {
-      if (head.value === previousValue) {
-        newNode.next = head.next;
-        head.next = newNode;  
-        break;     
-      }   
-    }
+  list.addValueBeforeValue = (previousValue, newValue) => {
+    let newNode = Node(newValue);
+    for (var head = list.head; head.value !== previousValue; head = head.next) {}
+    newNode.next = head.next;
+    head.next = newNode;  
+    return list;     
   };
 
   return list;
 };
 
-var Node = function(value) {
-  var node = {};
+let Node = value => {
+  let node = {};
 
   node.value = value;
   node.next = null;
